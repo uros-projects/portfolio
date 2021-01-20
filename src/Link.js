413270680ce31fe1d98de42fe2b1e81882593c76
Uros <uros.tomasevic.work@gmail.com>
2021-01-20 23:53:48 +0100

    Added project number in front of project name

--- src/Link.js
+++ src/Link.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Link = ({ project }) => {
-    const { name, url, description } = project;
+    const { id, name, url, description } = project;
 
     return (
         <section className='section'>
             <a href={`${url}`} target="_blank" rel="nofollow noopener noreferrer">
                 <div className='blog-preview'>
-                    <h2>{name}</h2>
+                    <h2>{id} - {name}</h2>
                     <p>{description}</p>
                 </div>
             </a>
